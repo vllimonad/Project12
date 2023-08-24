70bda0f7d4b36cc2f63a15df51504c60e1d433bb
Vlad Klunduk <klundukvlad40@gmail.com>
2023-08-24 13:27:48 +0200

    Designing UICollectionView cells

---
+++ project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Vlad Klunduk on 24/08/2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
